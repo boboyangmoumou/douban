5beba25508e3d55616138a742b7f1762caa3f90e
boboyangmoumou <18229635207@163.com>
2017-08-18 17:13:20 +0800

    addd

--- utils/foreach.js
+++ utils/foreach.js
@@ -58,21 +58,66 @@ function provessSubject(subject) {
 function moreInfo(temp){
     //标题
     var Title = temp.title;
+    temp.Title = Title;
+    //第二标题
+    var originTitle = temp.original_title;
+    temp.originTitle = originTitle;
+
     //地区
     var countries=temp.countries;
     var CountriesStr = '';
     for (var index in countries){
-      CountriesStr = CountriesStr + countries[index]+"/";
+      CountriesStr = CountriesStr + countries[index]+ "/";
+    }
+    if(CountriesStr != ""){
+      CountriesStr = CountriesStr.substring(0,CountriesStr.length -1);
     }
     //时间
     var Year = temp.year;
+    var Area = `${Year} ${CountriesStr}`;
+    temp.Area = Area;
+    console.log(Area);
     //简介
-    summary = temp.summary;
+    var summary = temp.summary;
+
+    //评分
+    var source = temp.rating.average;
     //类型
-    Type = temp.genres;
+    var Type = temp.genres;
     var TypeStr='';
-    for (let index in  Type){}
-    console.log(Title);
+    for (let index in  Type){
+      TypeStr = TypeStr + Type[index]+ "/"
+    }
+    if(TypeStr !=""){
+      TypeStr = TypeStr.substring(0,TypeStr.length -1);
+    }
+    //演员
+    var castsStr='';
+    var casts = temp.casts;
+    for(let index in temp.casts){
+      castsStr = castsStr + casts[index].name+ "/";
+    }
+    if(castsStr != ""){
+      castsStr = castsStr.substring(0, castsStr.length -1);
+    }
+
+    //导演
+    var directors = temp.directors;
+    var directorsStr = '';
+    for(let index in directors){
+      directorsStr = directorsStr + directors[index].name +"/";
+    }
+    if(directorsStr !=""){
+      directorsStr = directorsStr.substring(0,directorsStr.length - 1);
+    }
+    var moviesText = `评分: ${source} \n类型: ${TypeStr} \n演员: ${castsStr} \n导演: ${directorsStr}`
+    temp.moviesText = moviesText;
+    console.log(moviesText);
+    //图片
+    var Image = temp.images.large;
+    temp.Image = Image;
+
+
 }
 
 
